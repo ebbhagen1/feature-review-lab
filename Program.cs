bcdb47f593152a43140c3b02048451a03977c7ec
victor ebbhagen <ebbhagen1@gmail.com>
2026-09-23 15:02:38 +0200

    Add personalized welcome message

--- Program.cs
+++ Program.cs
@@ -1,10 +1,41 @@
-﻿namespace feature_review_lab
+﻿
+namespace feature_review_lab
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string? name;  // ? tecknet tillåter att vi kan lagra text som tom (null) (Variabeln namn)
+
+            while (true)//Oändlig loop 
+            {
+
+                Console.WriteLine("Vad heter du??"); //Frågar användaren om namn
+                name = Console.ReadLine(); //Väntar på svar från användaren
+
+                
+                if (string.IsNullOrWhiteSpace(name)) //kollar om användaren skrev tomt, mellanslag eller text
+
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Du måste skriva ett namn"); //Om villkoret är sant körs If-loopen. 
+                    
+                }
+
+                else   //Om villkoret är falskt (giltigt namn så hoppar den hit. 
+                    
+                {
+                    Console.WriteLine($"Välkommen, {name}!"); //Skriver ut ett välkomstmeddalnde med namn
+                    break; //Avslutar programmet. 
+
+                }
+                
+            }
         }
     }
 }
+
+
+
+
+
