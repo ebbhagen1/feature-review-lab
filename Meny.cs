d2c936ac349356dc96a5d31d927a3e8b9b00e700
victor ebbhagen <ebbhagen1@gmail.com>
2026-09-25 09:36:34 +0200

    Change Program.cs

---
+++ Meny.cs
@@ -0,0 +1,56 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace feature_review_lab
+//{
+//    internal class Meny
+//    {
+//        ResturantMenu myMenu = new ResturantMenu();
+
+//        myMenu.AddItem("Cesarsallad", 129.00m, "Starter");
+//            myMenu.AddItem("Oxfile med klyftpotatis", 349.00m, "Main course");
+//            myMenu.AddItem("Kladdkaka med grädde", 89.00m, "Dessert");
+
+//            myMenu.PrintMenu();
+//        }
+
+//    public class Menu //hanterar enskild rätt 
+//    {
+//        public string? Dish { get; set; }
+//        public decimal Price { get; set; }
+//        public string? Category { get; set; }
+
+//    }
+
+//    public class ResturantMenu
+//    {
+//        public List<Menu> Items { get; set; } = new List<Menu>(); //Auto-implementerad lista som lagrar alla alterntiv
+
+//        public void AddItem(string dish, decimal price, string category) //metod för att lägga till en rätt i menyn
+//        {
+
+//            Menu newItem = new Menu
+//            {
+//                Dish = dish,
+//                Price = price,
+//                Category = category
+//            };
+
+//            Items.Add(newItem);
+//        }
+//        public void PrintMenu() //Metod 
+
+//        {
+//            Console.WriteLine("Välkommen till PastaWorld!, här är vår meny:");
+//            foreach (var item in Items)
+//            {
+//                Console.WriteLine($" {item.Dish} | {item.Price} kr | {item.Category}");
+//            }
+//        }
+
+
+
+
+//    }
+//}
--- Program.cs
+++ Program.cs
@@ -8,83 +8,42 @@ namespace feature_review_lab
     {
         static void Main(string[] args)
         {
-            ResturantMenu myMenu = new ResturantMenu();
-
-            myMenu.AddItem("Cesarsallad", 129.00m, "Starter");
-            myMenu.AddItem("Oxfile med klyftpotatis", 349.00m, "Main course");
-            myMenu.AddItem("Kladdkaka med grädde", 89.00m, "Dessert");
-
-            myMenu.PrintMenu();
-        }
-
-        public class Menu //hanterar enskild rätt 
-        {
-            public string? Dish { get; set; }
-            public decimal Price { get; set; }
-            public string? Category { get; set; }
-
-        }
-
-        public class ResturantMenu
-        {
-            public List<Menu> Items { get; set; } = new List<Menu>(); //Auto-implementerad lista som lagrar alla alterntiv
-
-            public void AddItem(string dish, decimal price, string category) //metod för att lägga till en rätt i menyn
             {
+                //string? name;
 
-                Menu newItem = new Menu
+                while (true)
                 {
-                    Dish = dish,
-                    Price = price,
-                    Category = category
-                };
 
-                Items.Add(newItem);
-            }
-            public void PrintMenu() //Metod 
+                    Console.WriteLine("Vad heter du??");
+                    string? name = Console.ReadLine()?.Trim(); //väljer att lägga in trim för att skriva ut namnet utan mellanslag. 
 
-            {
-                Console.WriteLine("Välkommen till PastaWorld!, här är vår meny:");
-                foreach (var item in Items)
-                {
-                    Console.WriteLine($" {item.Dish} | {item.Price} kr | {item.Category}");
+
+                    if (string.IsNullOrWhiteSpace(name)) 
+
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Du måste skriva ett namn");
+
+                    }
+
+                    else
+
+                    {
+                        Console.WriteLine($"Välkommen, {name}!");
+                        break;
+                    }
+
                 }
             }
+
         }
     }
 }
-                             
-           
-
-        
-        //{
-        //    string? name;
-
-        //    while (true)
-        //    {
-
-        //        Console.WriteLine("Vad heter du??");
-        //        name = Console.ReadLine()?.Trim(); //väljer att lägga in trim för att skriva ut namnet utan mellanslag. 
 
 
-        //        if (string.IsNullOrWhiteSpace(name))
 
-        //        {
-        //            Console.WriteLine();
-        //            Console.WriteLine("Du måste skriva ett namn");
 
-        //        }
 
-        //        else
-
-        //        {
-        //            Console.WriteLine($"Välkommen, {name}!");
-        //            break;
-        //        }
-
-        //    }
-        //}
-       
 
 
 
